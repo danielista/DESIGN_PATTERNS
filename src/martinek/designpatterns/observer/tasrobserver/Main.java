49b4ea00754c56e0cb1378b0243d181acd632192
danielista <61905525+danielista@users.noreply.github.com>
2021-06-07 12:53:11 +0200

    OBSERVER TASR priority added

--- src/martinek/designpatterns/observer/tasrobserver/Main.java
+++ src/martinek/designpatterns/observer/tasrobserver/Main.java
@@ -9,7 +9,20 @@ public class Main {
         new O3faxServer(tasr);
         new O4facebookStatus(tasr);
 
-        System.out.println("prvá nova sprava v tasr");
-        tasr.setMessage("KANADA JE MAJSTER SVETA");
+        System.out.println("prvá nova sprava v tasr s prioritou 1");
+        tasr.setMessage("KANADA JE MAJSTER SVETA",1);
+
+        System.out.println("druhá nova sprava v tasr s prioritou 2");
+        tasr.setMessage("zajtra nám končí karanténa", 2);
+
+        System.out.println("tretia nova sprava v tasr s prioritou 3");
+        tasr.setMessage("Vianoce prichádzajú", 3);
+
+
+        System.out.println("tretia nova sprava v tasr s prioritou 4 a iné");
+        tasr.setMessage("Slovensko má nový plán obnovy", -1);
+
+
+
     }
 }
--- src/martinek/designpatterns/observer/tasrobserver/TASR.java
+++ src/martinek/designpatterns/observer/tasrobserver/TASR.java
@@ -1,33 +1,71 @@
 package martinek.designpatterns.observer.tasrobserver;
 
 
-import martinek.designpatterns.observer.Observer;
-
 import java.util.ArrayList;
 import java.util.List;
 
 // prišla nova sprava ..cez setter a potom notify ;) všetkym observatorom
 public class TASR {
     private List<Observerik> observers = new ArrayList<Observerik>();
+    private String message;
+    private int priority;
+
+
+    public int getPriority() {
+        return priority;
+    }
 
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-        notifyAllObservers();
+    // nastaví a príjme správu.. a dá o tom vedieť všetkým observerom
+
+    /*
+    1 all observers
+    2 email,fb
+    3 email
+    ine cislo nikto spravu neriesi ;)
+     */
+
+    // setting message and priority of sending ..
+    public void setMessage(String message, int priority) {
+        if(priority==1){
+            this.message = message;
+            notifyAllObservers();
+        }
+        else if(priority==2){
+            this.message = message;
+            for (int i = 0; i < observers.size(); i++) {
+                if(i==0 || i==3){
+                    Observerik observer = observers.get(i);
+                    observer.update();
+                }
+            }
+        }
+        else if(priority==3){
+            this.message = message;
+            Observerik observer = observers.get(0);
+            observer.update();
+        }
+        else System.out.println(" NOTHING HAPPENING ;)");
     }
 
     private void notifyAllObservers() {
-        for (Observerik observer : observers) {
+        for (int i = 0; i < observers.size(); i++) {
+            Observerik observer = observers.get(i);
             observer.update();
         }
     }
 
-    private String message;
+
 
     public void attach(Observerik klient) {
         observers.add(klient);
     }
+
+    public void remove(){
+
+    }
+
 }
